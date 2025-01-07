720fe642d28be479c27fe50b9c1fa9f68e0967ab
kuneleengus <almazekin@gmail.com>
2025-01-08 00:47:31 +0500

    Add advanced logging

--- config.py
+++ config.py
@@ -1,8 +1,22 @@
 from aiogram import Bot
 from aiogram.types import Message
 
-bot = Bot(token="7133511865:AAEvuZ_B02EjazcpeFNeFg4s7nOH6MTb_ts")
+class Config:
+    
+    def __init__(self) -> None:
+        self.bot_main = Bot(token="7133511865:AAEvuZ_B02EjazcpeFNeFg4s7nOH6MTb_ts")
+        self.bot_test = Bot(token="6736544153:AAEzji3KJM4C5qR1ljgfNL8ZzvgW0Zc4HKI")
 
-async def save_user(message: Message):
-    await bot.send_message(chat_id=-1001870427118, text=f"{message.from_user.username}\n{message.chat.id}\n{message.text}")
-    # return
+    def init_bot(self, flag: str):
+        match flag:
+            case "test": 
+                return self.bot_test
+            case _:
+                return self.bot_main
+
+    async def save_user(self, message: Message, bot: Bot):
+        await bot.send_message(chat_id=-1001870427118, text=f"{message.from_user.username}\n{message.chat.id}\n{message.text}")
+        return
+    
+
+
